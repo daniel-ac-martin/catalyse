ba45e6a68b18b46fe877867f9c9efb9dbf537193
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-06-02 19:42:20 +0100

    Move forms library into its own package

---
+++ lib/forms/src/form.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { FormikProps } from 'formik';
+
+const prettyPrint = obj => JSON.stringify(obj, undefined, 2);
+
+export type FormProps = JSX.IntrinsicElements["form"] & FormikProps<any>;
+
+export const Form: React.FC<FormProps> = props => (
+  <React.Fragment>
+    <form
+      action={props.action}
+      id={props.id}
+      method={props.method}
+      onReset={props.onReset}
+      onSubmit={props.onSubmit}
+    >
+      {props.children}
+    </form>
+    <hr />
+    <div className="width-one-half" style={{ float: 'left' }}>
+      <h2>State</h2>
+      <h3>values</h3>
+      <pre>
+        {prettyPrint(props.values)}
+      </pre>
+      <h3>touched</h3>
+      <pre>
+        {prettyPrint(props.touched)}
+      </pre>
+      <h3>errors</h3>
+      <pre>
+        {prettyPrint(props.errors)}
+      </pre>
+    </div>
+  </React.Fragment>
+);
+
+export default Form;
