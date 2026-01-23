3aaa38780f3f7da97cc8a298049b61a7ab64e9d1
Daniel A.C. Martin <github@daniel-martin.co.uk>
2026-01-23 17:11:00 +0000

    CI: Remove references to old directories

--- .npm/version.sh
+++ .npm/version.sh
@@ -13,18 +13,18 @@ pnpm recursive up --workspace "@${name}/*"
 
 # Update references in template files
 sed -i -E "s/workspace:([^*\^0-9]*)(\^)?[^*]+\"/workspace:\1\2${version}\"/g" \
-  {lib,lib-govuk}/*/skel/*/package.json*
+  lib/*/skel/*/package.json*
 sed -i -E "s/workspace:([\*\^])\{\{else\}\}(\^)?[^*]+\{\{\/if\}\}\"/workspace:\1{{else}}\2${version}{{\/if}}\"/g" \
-  {lib,lib-govuk}/*/skel/*/package.json*
+  lib/*/skel/*/package.json*
 
 # Update references in peerDependencies
 sed -i -E "s/\"@(${name})\/([^\"]*)\":([^\"]*)\"[^:]+\"/\"@\1\/\2\":\3\"^${version}\"/g" \
-  {lib,lib-govuk}/*/skel/*/package.json* \
-  {apps,components,components-internal,lib,lib-govuk}/*/package.json
+  lib/*/skel/*/package.json* \
+  {apps,components,lib}/*/package.json
 
 # Stage modified files
 cp pnpm-lock.yaml pnpm-lock-committed.yaml
 git add \
-  {lib,lib-govuk}/*/skel/*/package.json* \
-  {apps,components,components-internal,lib,lib-govuk}/*/package.json \
+  lib/*/skel/*/package.json* \
+  {apps,components,lib}/*/package.json \
   pnpm-lock-committed.yaml
