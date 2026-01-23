a7f8dbcbfcc6923f11038fe1bc473ffa136be7bf
Daniel A.C. Martin <github@daniel-martin.co.uk>
2026-01-23 17:14:35 +0000

    plop-pack: Simplify Vite config
    We don't need this extra config as we don't use govuk-react in this
    repo.

--- lib/plop-pack/skel/app/vite.config.ts
+++ lib/plop-pack/skel/app/vite.config.ts
@@ -9,28 +9,11 @@ export default defineConfig({
       defaultIsModuleExports: true // Mimics Node.js, aligns dev and prod
     }
   },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-        quietDeps: true, // Works around issues with govuk-frontend
-        silenceDeprecations: [
-          'if-function', // Required until if functionality is more common in browsers and SASS v1.95 gets a bit older
-          'import'       // Required until govuk-frontend moves to using modules
-        ]
-      }
-    }
-  },
   plugins: [
     html(),
     mdx(),
     reactRouter(),
   ],
-  resolve: {
-    alias: {
-      '@not-govuk/sass-base': '@not-govuk/sass-base/vite' // Vite resolves url() differently from Turbo/webpack
-    }
-  },
   ssr: {
     noExternal: /\.mdx$/
   }
